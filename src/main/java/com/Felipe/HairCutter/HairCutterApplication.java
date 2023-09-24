fc7e497960f7d537bc1e5979847292a365e30ebb
FelipeWeek066 <felipe1smg1g@gmail.com>
2023-09-24 17:44:28 -0400

    project Created

---
+++ src/main/java/com/Felipe/HairCutter/HairCutterApplication.java
@@ -0,0 +1,13 @@
+package com.Felipe.HairCutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairCutterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairCutterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Felipe/HairCutter/HairCutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Felipe.HairCutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairCutterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
